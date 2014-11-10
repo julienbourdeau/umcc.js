1a01070c1c7599ffbb34e037bbdfe0bd94e4f016
Julien Bourdeau <julien@sigerr.org>
2014-11-10 13:19:54 +0100

    Setting up basic stuff

---
+++ js/application.js
@@ -0,0 +1,8 @@
+window.Umcc = Ember.Application.create();
+
+var showdown = new Showdown.converter();
+Ember.Handlebars.helper('format-markdown', function(input) {
+  if (input === undefined)
+    return "";
+    return new Handlebars.SafeString(showdown.makeHtml(input));
+});
---
+++ js/router.js
@@ -0,0 +1,4 @@
+Umcc.Router.map(function() {
+  this.resource('umcc');
+  this.resource('rules', { path: '/rules' });
+});
